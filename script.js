62c6a32c1729cfd1c85abfa9afaec136fe2e62d0
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-11-01 13:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-let randomNumber = Math.floor(Math.random() * 3);
-let buttons = ["0", "1", "2"];
-let buttonsIndex = 0;
+let randomButton = Math.floor(Math.random() * 3);
+let buttonIndex;
 
-function randomWinner() {
-    if (buttonsIndex < 3) {
-        if (buttons[buttonsIndex] == randomNumber) {
-            alert("Winner");
-        } else {
-            alert("Loser");
-        }
+document.querySelectorAll("button").forEach((button, buttonIndex) => {
+  button.addEventListener("click", () => {
+    if (buttonIndex == randomButton) {
+      alert("Winner");
     } else {
-        alert("Game over! Please reload the page to play again");
+      alert("Loser");
     }
-    ++buttonsIndex;
-}
-
-
-//Creez 3 butoane
-//Generez un numar random in intervalul 0-2 inclusiv
-//In functie de numarul generat trebuie sa declar butonul castigator (ex. nr generat 2, al doilea buton este castigator)
-//Trebuie sa creez o functie care afiseaza un mesaj in momentul in care utilizatorul da click pe buton -> Castigator, daca butonul apasat de catre user este castigator sau pierzator pentru celelalte doua butoane
+  });
+});
