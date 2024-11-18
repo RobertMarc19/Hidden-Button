0067f8ff1d241565fa2dfb6a523671ec40c2e650
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-11-18 13:07:50 +0200

    Upgraded verions of Hidden Buttons

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let randomButton = Math.floor(Math.random() * 3);
-let buttonIndex;
-
-document.querySelectorAll("button").forEach((button, buttonIndex) => {
-  button.addEventListener("click", () => {
-    if (buttonIndex == randomButton) {
-      alert("Winner");
-    } else {
-      alert("Loser");
-    }
-  });
-});
+function generateButtons() {
+  let nrOfButtons = document.getElementById("introducedNumber").value;
+  let container = document.getElementById("buttonsContainer");
+  let randomButton = Math.floor(Math.random() * nrOfButtons);
+  container.innerHTML = "";
+  for (let i = 0; i < nrOfButtons; ++i) {
+    let button = document.createElement("button");
+    button.textContent = `Button ${i + 1}`;
+    container.appendChild(button);
+    button.addEventListener("click", () => {
+      if (i == randomButton) {
+        alert("Winner");
+      } else {
+        alert("Loser");
+      }
+    });
+  }
+}
